c8b7363b9cf85d029c1255b610532895330302af
mvanacker <maurits.vanacker@gmail.com>
2020-08-29 17:57:39 +0200

    Hello, world!

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
+import { Button } from "@material-ui/core";
+
+export default () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="hello-world-container">
+      <Button variant="contained" color="primary">
+        Hello, world!
+      </Button>
     </div>
   );
-}
-
-export default App;
+};
